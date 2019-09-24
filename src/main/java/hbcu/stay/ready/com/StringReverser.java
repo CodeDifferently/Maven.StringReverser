481998e787e78cd91a4e80f4000e7fbd6e4be874
git-leon <xleonhunter@gmail.com>
2019-09-24 18:35:58 -0400

    update

---
+++ src/main/java/hbcu/stay/ready/com/StringReverser.java
@@ -0,0 +1,7 @@
+package hbcu.stay.ready.com;
+
+public class StringReverser {
+    public static String reverse(String stringToReverse) {
+        return null;
+    }
+}
---
+++ src/test/java/hbcu/stay/ready/com/StringReverserTest.java
@@ -0,0 +1,58 @@
+package hbcu.stay.ready.com;
+
+import org.junit.Test;
+
+public class StringReverserTest {
+    @Test
+    public void test1() {
+        // given
+        String input = "Hello";
+        String expected = "olleH";
+
+        // when
+        String actual = StringReverser.reverse(input);
+
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test2() {
+        // given
+        String input = "Jello";
+        String expected = "olleJ";
+
+        // when
+        String actual = StringReverser.reverse(input);
+
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        // given
+        String input = "Reverse";
+        String expected = "esreveR";
+
+        // when
+        String actual = StringReverser.reverse(input);
+
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void test4() {
+        // given
+        String input = "One more time";
+        String expected = "emit erom enO";
+
+        // when
+        String actual = StringReverser.reverse(input);
+
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+}
